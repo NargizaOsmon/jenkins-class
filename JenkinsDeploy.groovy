dfe480ad586feb63fb5bd9b025793666e531cbee
NargizaOsmon <nargizaosmon@gmail.com>
2020-09-12 16:11:15 -0500

    Added stages to deploy job

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -1,3 +1,6 @@
+// Uniq name for the pod or slave 
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+
 properties([
     [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
     parameters([
@@ -14,6 +17,63 @@ println(
     Destroy changes: ${params.DestroyChanges}
     Docker image: ${params.selectDockerImage}
     Environment: ${params.environment}
-
     """
 )
+
+// Creating yaml definition for slaves
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+
+        - name: terraform
+          image: hashicorp/terraform:0.12.27
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+      node(k8slabel) {
+
+        stage("Pull the SCM") {
+            git 'https://github.com/NargizaOsmon/jenkins-class.git'
+        }
+
+        stage("Apply/Plan") {
+            container("fuchicorptools") {
+                sh 'kubectl version'
+            }
+        }
+      }
+    }
